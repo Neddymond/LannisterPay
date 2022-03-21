8e5cfe6425e2f4f2c04c04484ec32b5e599eaaca
Neddymond <chinedu@ext.kobo360.com>
2022-03-21 10:12:13 +0100

    Add heroku connection url

--- src/db/models/redis.js
+++ src/db/models/redis.js
@@ -1,19 +1,17 @@
 const config = require('../../config/config');
 const redis = require('redis');
-let client = redis.createClient();;
+let client;
+
+if (process.env.REDISCLOUD_URL) {
+  client = redis.createClient();
+} else {
+  client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+}
 
 exports.initRedis = async () => {
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
   client.on('connect', () => console.log('client connected successfully'));
-
-  // await client.hSet("car", "car.name", "Toyota");
-  // await client.hSet("car", "colour", "Red");
-  // await client.hSet("car", "owner", "Chinedu");
-  // await client.hSet("car", "key", "Custom");
-  // await client.hSet("car", "car.model", "Latest");
-  // const value = await client.hGetAll("car");
-  // console.log('redis keyyyy ====> ', value);
 };
 
 exports.redisClient = client;
